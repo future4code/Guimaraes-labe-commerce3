210b9da50dd1963755f4405414999a64fd842b45
Robson R Ribeiro <projetos.r@hotmail.com>
2022-02-13 12:18:46 -0300

    Criação do component produto

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
+//css
+import * as S from './styles';
+
+//assets
+import Camiseta from '../../assets/Back.webp'
+
 class Produto extends React.Component{
     render(){
-        return(<>Produtos</>)
+        return(
+            <S.Container>
+                <S.ContainerProduto>
+                    <S.Title>Camiseta espacil H4P5F</S.Title>
+                </S.ContainerProduto>
+                
+                {/* <S.ProdutoDescription>Camiseta produzida pela NASA</S.ProdutoDescription>  */}
+
+                {/* {/* <span>R$ 18,00</span> */}
+                <S.Imagem 
+                    src={Camiseta} 
+                    alt="camiseta"
+                />
+
+                <S.InfoMoney>R$ 1.000,00</S.InfoMoney>
+            
+            </S.Container>
+        )
     }
 }
 
