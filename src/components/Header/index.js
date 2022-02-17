b06ed2982690cc1a5b24fbedafc5d61062e8652f
AlexBitenc0urt <alexx.bittencourt@gmail.com>
2022-02-17 16:56:21 -0300

    add logo e campo pesquisa do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,49 @@
 import React,{ useState } from 'react';
 
 import styled from 'styled-components';
+import LogoAmazon from '../../assets/logo-amazon'
+import IconeLupa from '../../assets/icone-lupa'
 
 //icon
 import { BsFillCartCheckFill } from 'react-icons/bs';
 
 
 const Container = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-content: space-between;
     width: auto;
     height: 70px;
+    border: 1px solid black;
+    background-color: #6D6DF2; 
 
-    /* border: 1px solid black; */
     
+`;
+
+const Logo = styled.div`
+    display: flex;
+    padding: 1px 0;
+
+    img{
+        height: 60px;
+        width: 70px;
+    }
+    
+
+    
+`;
+
+const Pesquisa = styled.div`
+    display: flex;
+    height: 35px;
+    padding: 15px 0;
+    border-radius: 0.5em; 
+    justify-content: center;
+
+    img{
+        height: 20px;
+        width: 20px;
+    }
 `;
 
 
@@ -30,11 +62,21 @@ const CarrinhoCompras = styled.div`
 `;
 
 
+
 class Header extends React.Component{
     render(){
         const item = localStorage.getItem('itemCarrinho');
         return(<>
             <Container className='header'>
+                    <Logo className='logo'>
+                        <img src={LogoAmazon}/>
+                    </Logo>
+
+                    <Pesquisa className='pesquisa'>
+                        <input type="texto" name='pesquisa' id='pesquisa' placeholder='Faça sua busca' required/>
+                        <button><img src={IconeLupa}/> </button>            
+                    </Pesquisa>
+
                     <CarrinhoCompras className='carrinho'>
                         <strong>{item? item : 0 }</strong>
                         <a href='#'><BsFillCartCheckFill size={40}/></a>
