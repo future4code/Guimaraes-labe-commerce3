ce5ab428773dd88afbefec7b8e5a591120d452d3
Levi Nakayama <levisn@gmail.com>
2022-02-18 19:19:44 -0300

    adicionado 2 listas de produtos e localstorage no carrinho

---
+++ src/utils/Camisetas.js
@@ -0,0 +1,91 @@
+export const Camisetas = [
+    {
+        id: 1,
+        categoria: 'Camisetas',
+        title: 'Camiseta NASA Branca',
+        price: '100,00',
+        url: 'https://www.visionmerch.com/wp-content/uploads/2020/09/NASA-Logo-T-shirt-72dpi.jpg',
+        qtd: 0,
+    },
+    {
+        id: 2,
+        categoria: 'Camisetas',
+        title: 'Camiseta Space Force Branca',
+        price: '80,00',
+        url: 'https://cdn7.bigcommerce.com/s-90c91/images/stencil/1280x1280/products/3299/7636/space_force_white_unisex__30150.1529597618.png?c=2&imbypass=on',
+        qtd: 0,
+    },
+
+    {
+        id: 4,
+        categoria: 'Camisetas',
+        title: 'Camiseta NASA Azul',
+        price: '100,00',
+        url: 'https://teenamycs.com/wp-content/uploads/2020/01/Nasa-Aeronautics-And-Space-Administration-T-Shirt.jpg',
+        qtd: 0,
+    },
+
+    {
+        id:5,
+        categoria: 'Camisetas',
+        title: 'Camiseta NASA Vermelha',
+        price: '100,00',
+        url: 'https://inkteeshop.com/wp-content/mockup/Nasa-Official-Logo-In-The-Stars-Graphic_Premium-T-shirt_Red.JPEG',
+        qtd: 0,
+    },
+
+    {
+        id: 6,
+        categoria: 'Camisetas',
+        title: 'Camiseta NASA Logo Preta',
+        price: '110,00',
+        url: 'https://images.sportsdirect.com/images/imgzoom/59/59811103_xxl.jpg',
+        qtd: 0,
+    },
+
+    {
+        id: 7,
+        categoria: 'Camisetas',
+        title: 'Camiseta NASA Logo Tridimensional Preta',
+        price: '110,00',
+        url: 'https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140,2000%7C91t-8V22zyL.png%7C0,0,2140,2000+0.0,0.0,2140.0,2000.0._UL1500_.png',
+        qtd: 0,
+    },
+
+    {
+        id: 8,
+        categoria: 'Camisetas',
+        title: 'Camiseta NASA Logo Central Preta',
+        price: '110,00',
+        url: 'https://i.ebayimg.com/images/g/GugAAOSwDwJdMXTw/s-l300.png',
+        qtd: 0,
+    },
+
+    {
+        id: 9,
+        categoria: 'Camisetas',
+        title: 'Camiseta NASA PRETO Branca',
+        price: '100,00',
+        url: 'https://media3.remixline.com/15301-medium_default/nasa-logo-imprime-black-logo-tshirt-blanc.jpg',
+        qtd: 0,
+    },
+
+    {
+        id: 10,
+        categoria: 'Camisetas',
+        title: 'Camiseta NASA Manga Longa Branca',
+        price: '150,00',
+        url: 'https://media3.remixline.com/13927-small_default/nasa-logo-imprime-tshirt-manches-longues-blanc.jpg',
+        qtd: 0,
+    },
+
+    
+    {
+        id: 11,
+        categoria: 'Camisetas',
+        title: 'Camiseta NASA VERMELHO Branca',
+        price: '100,00',
+        url: 'https://cdn.shopify.com/s/files/1/1765/7665/products/4eb1e37079674981fedb010813f7c75a_x700.jpg?v=1544714086',
+        qtd: 0,
+    },
+]
